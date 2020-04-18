49a9025a97e2f4ce38bae9b74a6f6bb5cf6a1915
Umuhire-H <umuhire.dev@gmail.com>
2020-04-19 00:28:17 +0200

    ParticipationType issue : SOLVED

--- src/Form/ParticipationType.php
+++ src/Form/ParticipationType.php
@@ -3,23 +3,55 @@
 namespace App\Form;
 
 use App\Entity\Participation;
+use App\Entity\ActivityExecution;
+use App\Repository\ActivityExecutionRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class ParticipationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('datePayement')
-            ->add('pricePayed')
-            ->add('typePayement')
-            ->add('statusPayement')
-            ->add('comment')
-            ->add('activityExecution')
-            ->add('user')
-            ->add('child')
+            ->add('datePayement', DateTimeType::class)
+            ->add('pricePayed', MoneyType::class)
+            ->add('typePayement', ChoiceType::class,[//select
+                'choices' => [
+                    'cash' => 'cash',
+                    'paypal' => 'paypal',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'required' => true
+            ])
+            ->add('statusPayement', ChoiceType::class, [ //radio
+                'choices' => [
+                    'en cours' => 'en cours',
+                    'payé' => 'payé',
+                    'non payé' => 'non payé',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'required' => true,
+            ])
+            ->add('comment', TextType::class)
+            ->add('activityExecution', EntityType::class, [
+                'class' => ActivityExecution::class,
+                'query_builder' => function (ActivityExecutionRepository $repo){
+                    return $repo->createQueryBuilder('ae');
+                },
+                'choice_label' => function($ActEx){
+                    return $ActEx->getActivity()->getName();
+                }
+            ]);
+            // ->add('user')
+            // ->add('child')
         ;
     }
 
